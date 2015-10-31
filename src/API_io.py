38c5bd491f78a723ae769df55f27557f24b3c6e3
Michael Patterson <map222@gmail.com>
2015-10-31 13:57:35 -0700

    Refactored API code
    API code refactored to use requests.get parameters (created dictionaries
    to pass to requests.get). Moved the match history JSON parsing into the
    module. Used string.formate for urls in some functions.
    Created a notebook to scrape people from different ELOs. This could
    probably be functionalized to be more general.

--- src/API_io.py
+++ src/API_io.py
@@ -9,7 +9,7 @@ import numpy as np
 import itertools
 import pdb
 
-def load_featured_games( api_key, region = 'na'):
+def load_featured_games( api_key, region_key = 'na'):
     """ Loads the game IDs of the "featured games"
     
         Argument:
@@ -19,83 +19,108 @@ def load_featured_games( api_key, region = 'na'):
         featured_game_ids: List of ints of game IDs
     """
     
-    featured_url = 'https://' + region + '.api.pvp.net/observer-mode/rest/featured' + '?api_key=' + api_key
-    featured_json = requests.get(featured_url).json()['gameList'] # load the url of featured games, and parse the JSON
-    return featured_json
+    api_dict = {'api_key': api_key}
     
-def get_summoners_IDs_from_featured_games(featured_json, api_key, region = 'na'):
+    featured_url = 'https://' + region_key + '.api.pvp.net/observer-mode/rest/featured'
+    featured_request = requests.get(featured_url, params = api_dict) # load the url of featured games, and parse the JSON
+    return featured_request.json()['gameList']
+    
+def get_summoners_IDs_from_featured_games(featured_json, api_key, region_key = 'na'):
     """
-    Load the summoner names and summoner IDs from the featured game JSON
+    Load the summoner names from the featured game JSON. (This JSON has names and not IDs).
+    Then send summonerNames to the API to get summoner IDs.
     
     Arguments:
     featured_json: JSON output of load_featured_games
     api_key: string of your api_key for the request
     
     Returns:
-    participant_names
+    summoner_names, summoner_IDs
     """
     
     participants_json = np.array([x['participants'] for x in featured_json]).ravel() # get participants JSON for each game
-    participant_names = [x['summonerName'] for x in participants_json] # strip out summoner names
+    summoner_names = [x['summonerName'] for x in participants_json] # strip out summoner names
     
     # get summoner IDs for the participants:
+    chunked_names = list(urlify_string_list(summoner_names))
+    summoner_ids = [get_summoner_ids_from_names(x, api_key, region_key) for x in chunked_names ]
     
-    summoner_ids = [get_summoner_ids_from_names(x, api_key, region) for x in list(urlify_string_list(participant_names)) ]
+    summoner_ids = list(itertools.chain.from_iterable(summoner_ids)) # flatten nested list
     
-    summoner_ids = list(itertools.chain.from_iterable(summoner_ids)) # then flatten them
-    
-    return participant_names, summoner_ids
-
-def get_summoner_ids_from_names(participant_names_single_string, api_key, region = 'na'):
-    """ Returns the summoner ids for a list of summoner names
-    """
-    summoner_url = 'https://'+region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + \
-                    participant_names_single_string + '?api_key=' + api_key
-                    
-    summoner_info = requests.get(summoner_url).json()
-    
-    summoner_ids = [x['id'] for x in list(summoner_info.values())]
-    return summoner_ids
+    return summoner_names, summoner_ids
 
 def urlify_string_list(list_of_string):
-    """ Takes a list of strings, and turns into a single, long, comma separated string for url comprehension
+    """ Takes a list of strings, and turns into a single, long, comma separated string for url comprehension.
+        This is done because the Riot API only accepts 40 summoner names per query.
     """
     max_summoners = 40 # maximum number of summoners you can query at once with the
     for i in range(0, len(list_of_string), max_summoners):
         yield ','.join(list_of_string[i:i+max_summoners])
 
-def make_matchlist_url_summoner_ID(cur_ID, api_key, solo_ranked_flag = True, season_flag = True, region = 'na'):
-    """ Creates a request url for the given summoner ID, and the flags
+def get_summoner_ids_from_names(participant_names_single_string, api_key, region_key = 'na'):
+    """ Returns the summoner ids for a list of summoner names
+    """
+    
+    # create parameters for the request
+    api_dict = {'api_key': api_key}
+    summoner_url = 'https://'+region_key + '.api.pvp.net/api/lol/' + region_key + '/v1.4/summoner/by-name/' + \
+                    participant_names_single_string
+                    
+    summoner_info = requests.get(summoner_url, params = api_dict).json()
+    
+    summoner_ids = [x['id'] for x in list(summoner_info.values())]
+    return summoner_ids
+
+def make_matchhistory_url(summoner_ID, region_key = 'na'):
+    """ Creates a url for passage to requests.get to get all matches by a single player.
+        In use, should be combined with make_RIOT_request_params
+    """
+    base_url = 'https://{0}.api.pvp.net/api/lol/{0}/v2.2/matchlist/by-summoner/{1}'
+        
+    return base_url.format(region_key, summoner_ID)
+
+def make_RIOT_request_params( api_key, solo_ranked_flag = False, season_flag = False, timeline_flag = False):
+    """ Creates a dictionary to pass to request.get containing parameters for whether we want ranked games,
+        season, and the api_key
+        
+        Called in conjuction with urls made by make_matchlist_url_summoner_ID or make_match_info_url
     
     Arguments:
-    list_IDs: a python list of summoner IDs (ints)
     solo_ranked_flag: whether to only get solo Queue games (when care about getting games for ML)
     season_flag: Whether to get games from only a particular season (for now, Season 2015)
-    
-    Returns: ??
     """
-    if solo_ranked_flag:
-        solo_ranked_string = 'rankedQueues=RANKED_SOLO_5x5&'
-    else:
-        solo_ranked_string = ''
-        
-        
-    if season_flag:
-        season_string = 'seasons=SEASON2015&'
-    else:
-        season_string = ''
-        
-    return 'https://' + region+ '.api.pvp.net/api/lol/' + region + '/v2.2/matchlist/by-summoner/' + \
-                        str(cur_ID) + '?' + solo_ranked_string + season_string + 'api_key=' + api_key
-
-def make_match_info_url(match_ID, timeline_flag, api_key, region = 'na'):
-    if timeline_flag:
-        timeline_string = 'includeTimeline=true&'
-    else:
-        timeline_string = ''
-    return 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v2.2/match/' + str(match_ID) + '?' + \
-            timeline_string + 'api_key=' + api_key
     
+    request_param_dict = {'api_key': api_key}
+    
+    if solo_ranked_flag:
+        request_param_dict['rankedQueues'] = 'RANKED_SOLO_5x5'        
+    if season_flag:
+        request_param_dict['seasons'] = 'SEASON2015'
+    if timeline_flag:
+        request_param_dict['includeTimeline'] = 'true'
+        
+    return request_param_dict
+
+def parse_match_json_for_matchIDs(match_histories, region_key):
+    """ Loop through a list of JSONs, each of which is the match history for a single player.
+    
+    Returns: a numpy array list of int matchIDs
+    """
+    match_IDs = np.empty(0, dtype=int)
+    for cur_matches in match_histories:
+        if cur_matches['totalGames'] > 0: # for some summoners, for whatever reason, the totalGames will be 0
+            match_IDs = np.append( match_IDs, [x['matchId'] for x in cur_matches['matches'] if x['region'] == region_key.upper()] )
+    match_IDs = np.unique(match_IDs)
+    return match_IDs
+
+def make_match_info_url(match_ID, region_key = 'na'):
+    """ Creates a url for passage to requests.get to get all matches by a single player.
+        In use, should be combined with make_RIOT_request_params
+    """
+        
+    base_url = 'https://{0}.api.pvp.net/api/lol/{0}/v2.2/match/{1}'
+    return base_url.format(region_key, match_ID)
+
 import time
 def rate_limited(maxPerSecond):
     minInterval = 1.0 / float(maxPerSecond)
@@ -113,10 +138,10 @@ def rate_limited(maxPerSecond):
     return decorate
 
 @rate_limited(0.8)
-def get_limited_request(request_url):
+def get_limited_request(request_url, request_params):
     num_tries = 10 # try a set number of times
     for i in range(num_tries):
-        cur_request = requests.get(request_url)
+        cur_request = requests.get(request_url, request_params)
         if cur_request.status_code == 200:
             return cur_request.json()
         else:
